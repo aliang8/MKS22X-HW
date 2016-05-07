f87cfbf48522c03d0da8b09e6a871d06ec8ceab1
Anthony Liang <aliang8@stuy.edu>
2016-05-07 00:04:39 -0400

    Work stuff

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -3,15 +3,24 @@ import java.util.*;
 public class MyHeap<T extends Comparable<T>>
 {
    private int size;
+   private boolean isMax;
    private T[] data;
 
-   public MyHeap(){}
+   public MyHeap(){
+	data = (T)new Comparable[10];
+	isMax = true;
+	size = 0; 
+   }
+   public MyHeap(boolean max){
+	heap = (T)new Comparable[10];
+	isMax = max;
+	size = 0;
+    }
    public MyHeap(T[] array){
-       size = array.length + 1;
-       data = new T[size];
-       for (int i = 0; i < size; i++){
-	   data[i] = array[i-1];
-       }
+        size = array.length;
+        data = array;
+       	heapify();
+	isMax = true;
    }
    private void pushDown(int k){
        if (data[k].compareTo(data[2*k-1])< 0){
@@ -23,10 +32,28 @@ public class MyHeap<T extends Comparable<T>>
 	   
    private void pushUp(int k){
        if (data[k].compareTo(data[k/2
-   private void heapify()
+   private void heapify(
+	T [] temp = (T[])new Comparable[data.length*2+1];
+	int start = size/2-1;	
+	for (int i = start; i > 1; i--) {
+	    if (compare(data[i].compareTo(data[i/2]))) {
+		pushDown(i);
+	    }
+	}
+	for (int i = 0; i < data.length; i++) {
+	    temp[i+1] = data[size-i-1];
+	}
+	data = temp;
+   )
    public T delete()
    public void add(T x)
-   private void doubleSize()
+   private void doubleSize(
+	T[]temp = (T[])new Comparable[data.length*2];
+	for (int i = 1; i <= size; i++) {
+	    temp[i] = data[i];
+	}
+	data = temp;
+   )
    public String toString()
 
    //do this last
