3a35bf82b8f9d1ee3eaf4bd8fb57182e79c3e2bf
Anthony Liang <aliang8@stuy.edu>
2016-05-08 23:28:51 -0400

    Finished, I didn't use Comparable

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,114 +1,91 @@
 import java.util.*;
-@SuppressWarnings("unchecked")
-public class MyHeap<T extends Comparable<T>>
-{
-   private int size;
-   private boolean isMax;
-   private T[] data;
-
-   public MyHeap(){
-	data = (T[])new Comparable[10];
-	isMax = true;
-	size = 0; 
-   }
-   public MyHeap(boolean max){
-	data = (T[])new Comparable[10];
-	isMax = max;
-	size = 0;
+public class MyHeap{
+    private int[] heap;
+    boolean isMax;
+    
+    public MyHeap(){
+	this(true);
     }
-   public MyHeap(T[] array){
-        size = array.length;
-        data = array;
-       	heapify();
-	isMax = true;
-   }
-   private void pushDown(int k){
-	T temp = data[k];
-	//System.out.println(size);
-	if (size >= k*2+1 && compare(data[k*2+1].compareTo(data[k*2]))) {
-	    data[k] = data[k*2];
-	    data[k*2] = temp;
-	}
-	else {
-	    data[k] = data[k*2+1];
-	    data[k*2+1] = temp;
-	    
-	}
-   }
-   private void pushUp(int pos) {
-	if(pos>1&&compare(data[pos],data[pos/2])){
-	    T temp=data[pos/2];
-	    data[pos/2]=data[pos];
-	    data[pos]=temp;
-	    pushUp(pos/2);
-	}
-   }
-   
-   private void heapify(){
-	T [] temp = (T[])new Comparable[data.length*2+1];
-	int start = size/2-1;	
-	for (int i = start; i > 1; i--) {
-	    if (compare(data[i].compareTo(data[i/2]))) {
-		pushDown(i);
-	    }
-	}
-	for (int i = 0; i < data.length; i++) {
-	    temp[i+1] = data[size-i-1];
-	}
-	data = temp;
-   }
-   //public T delete()
-   public void add(T value){
-	if (size+1 >= data.length) {
-	    doubleSize();
-	}
-	data[size+1] = value;
-	size++;
-	pushUp(size);
+    
+    public MyHeap(boolean isMax){
+	heap = new int[5];
+	this.isMax = isMax;
     }
 
-   private void doubleSize(){
-	T[]temp = (T[])new Comparable[data.length*2];
-	for (int i = 1; i <= size; i++) {
-	    temp[i] = data[i];
-	}
-	data = temp;
-   }
-   private boolean compare(int n) {
-	if (isMax) {
-	    return n < 0;
-	}
-	return n > 0;
+    public int remove(){
+	int ret = heap[1];
+	int val = heap[heap[0]];
+	heap[heap[0]] = 0;
+	heap[1] = val;
+	heap[0] = heap[0] - 1;
+	pushDown(1);
+	return ret;
     }
-   public String toString(){
-	String s = "[";
-	for (int i = 1; i <= size; i++) {
-	    if (i == size) {
-		s += data[i];
-	    }
-	    else {
-		s += (data[i] + ", ");
+    
+    public void pushDown(int index){
+	if ( index < heap[0] && ( !compare(index, index*2) || !compare(index, index*2+1) ) ){
+	    if ( compare(index*2,index*2+1) ){
+		swap(index,index*2);
+		pushUp(index*2);
+	    }else{
+		swap(index,index*2+1);
+		pushUp(index*2+1);
 	    }
 	}
-	s += "]";
-	return s;
     }
 
-    public static void main(String[]args) {
-	MyHeap<Integer> a = new MyHeap<>();
-	a.add(56);
-	System.out.println(a);
-	a.add(40);
-	a.add(35);
-	a.add(87);
-	a.add(7);
-	a.add(27);
-	a.add(18);
-	System.out.println(a);
-	//a.delete();
-	System.out.println(a);
-	Integer [] r = {7,18,27,35,40,56,87};
-	MyHeap<Integer> b = new MyHeap<>(r);
-	System.out.println(b);
+    public void add(int value){
+	if ( heap[0] == heap.length - 1){
+	    heap = Arrays.copyOf(heap, heap.length*2);
+	}
+	heap[0] = heap[0] + 1;
+	heap[heap[0]] = value;
+	pushUp(heap[0]);
+    }
+
+    public void pushUp( int index ){
+	if ( index != 1 && compare(index,index/2) ){
+	    swap(index,index/2);
+	    pushUp(index/2);
+	}
+    }
+
+    public void swap(int index1, int index2){
+	int current = heap[index1];
+	heap[index1] = heap[index2];
+	heap[index2] = current;
+    }
+
+    public boolean compare(int a , int b ){
+	if ( isMax ){
+	    return heap[a] > heap[b];
+	}
+	return heap[a] < heap[b];
+    }
+
+    public int peek(){
+	return heap[1];
+    }
+
+    public int getSize(){
+	return heap[0];
+    }
+
+    public String toString(){
+	return Arrays.toString(heap);
+    }
+
+    public static void main ( String [] args ){
+	MyHeap h = new MyHeap(true);
+	h.add(10);
+	h.add(60);
+	h.add(600);
+	h.add(1);
+	h.add(-100);
+	System.out.println(h);
+        h.remove();
+	System.out.println(h);
+	h.remove();
+	System.out.println(h);
     }
 }
