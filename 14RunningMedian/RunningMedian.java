e3d9a227e6d234a3c37c50a8ab26444975f9c0f4
Anthony Liang <aliang8@stuy.edu>
2016-05-10 20:37:03 -0400

    finished

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,28 +1,64 @@
-import java.util.*
+import java.util.*;
+
 public class RunningMedian{
-    MyHeap<Integer> minHeap= new MyHeap<>(false);
-    MyHeap<Integer> maxHeap= new MyHeap<>(true);
-    double median;
-    public RunningMedian(){			 
+    private MyHeap<Integer> min;
+    private MyHeap<Integer> max;
+    private double median;
+
+    public RunningMedian(){
+	min=new MyHeap<Integer>();
+	max=new MyHeap<Integer>(false);
     }
-	
+
     public double getMedian(){
-	if (minHeap.size() == 0 && maxHeap.size() == 0){
-	    throw new NoSuchElementException;
-	} else if (!median == null){
-	    return median;
-	} else {
-	    return (minHeap.peek() + maxHeap.peek()) / 2;
+	if(min.size() == 0 && max.size() == 0){
+	    throw new NoSuchElementException();
+	}
+	if (min.size() == max.size()){
+	    return (min.peek() + max.peek()) / 2.0;
+	}else if (min.size() > max.size()){
+	    return min.peek();
+	}else{
+	    return max.peek();
 	}
     }
-			   
-    public void add(Integer x){
-	if (x < median){
-	    minHeap.add(x);
+
+     public void add(Integer x) {
+	if (min.size() == 0 && max.size() == 0) {
+	    min.add(x);
+	    median = x;
 	} else {
-	    maxHeap.add(x);
+	    if (x < median) {
+		min.add(x);
+	    } else {
+		max.add(x);
+	    }
+	    while (min.size() - max.size() > 1) {
+		max.add(min.delete());
+	    }
+	    while (max.size() - min.size() > 1) {
+		min.add(max.delete());
+	    }
+	    getMedian();
 	}
+    }
+
+    public static void main(String[]args){
+	RunningMedian r = new RunningMedian();
+	r.add(34);
+	System.out.println(r.getMedian());
+	r.add(2);
+	System.out.println(r.getMedian());
+	r.add(8);
+	System.out.println(r.getMedian());
+	r.add(2);
+	System.out.println(r.getMedian());
+	r.add(15);
+	System.out.println(r.getMedian());
+	r.add(0);
+	System.out.println(r.getMedian());
+	r.add(12);
+	System.out.println(r.getMedian());
     }
 }
-
 
